871266f0e89fbe91075e8d1391ab5b25b1ffcc5a
haydenyoung <hayden@knowledgearc.com>
2017-07-11 04:23:46 +0800

    Article ingestion moved to system.

--- plugins/ingest/article/sql/install.mysql.utf8.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__ingest_articles` (
-    `content_id` INTEGER COMMENT 'The article id.',
-    `item_id` VARCHAR(128) COMMENT 'The external id of the item.',
-    `created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-    `modified` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-    PRIMARY KEY (`content_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- plugins/ingest/article/sql/uninstall.mysql.utf8.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `#__ingest_articles`;
