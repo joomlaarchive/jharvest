1e15e53b7191e4e05fb6d2bf4f805c61fd374861
haydenyoung <hayden@knowledgearc.com>
2017-08-14 03:19:41 +0800

    Remove sql tables; do not record destination records. Only ingest one record at a time as defined by the trigger. Crosswalk data.

--- plugins/ingest/dspace/dspace.php
+++ plugins/ingest/dspace/dspace.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * @package     Ingest.DSpace.Plugin
+ *
  * @copyright   Copyright (C) 2014-2017 KnowledgeArc Ltd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
@@ -11,107 +13,52 @@ JLoader::import('joomla.filesystem.folder');
 /**
  * Ingests metadata and assets into DSpace.
  *
- * @package     JHarvest.Plugin
+ * @package  Ingest.DSpace.Plugin
  */
 class PlgIngestDSpace extends JPlugin
 {
     protected $autoloadLanguage = true;
 
-    public function __construct($subject, $config = array())
-    {
-        parent::__construct($subject, $config);
-
-        \JLog::addLogger(array());
-    }
-
     /**
-     * Gets the cached records belonging to this harvest.
+     * Ingest a harvested item.
      *
-     * The cache can be returned in chunks to avoid performance issues.
-     *
-     * @param   int        $start  The cache offset.
-     * @param   int        $limit  The size of the cache to return.
-     *
-     * @return  JObject[]  An array of cached records.
+     * @param  stdClass   $item    The harvested item.
+     * @param  JRegistry  $params  The harvester params.
      */
-    public function getCache($start = 0, $limit = 100)
+    public function onJHarvestItemIngest($item, $params)
     {
-        $database = \JFactory::getDbo();
+        $this->params->merge($params);
 
-        $query = $database->getQuery(true);
+        $data = json_decode($item->data);
+        
+        $metadata = $data->metadata;
+        $assets = $data->assets;
 
-        $select = array(
-            $database->qn('id'),
-            $database->qn('harvest_id'),
-            $database->qn('data'));
+        $collection = $params->get('ingest.dspace.collection');
 
-        $query
-            ->select($select)
-            ->from($database->qn('#__jharvest_cache', 'cache'))
-            ->where($database->qn('cache.harvest_id').'='.(int)$this->harvestId);
+        $path = $this->buildPackage($item->id, $collection, $metadata, $assets);
 
-        $database->setQuery($query, $start, $limit);
+        $http = JHttpFactory::getHttp(null, 'curl');
 
-        return $database->loadObjectList('id');
-    }
+        $headers = array(
+            'user'=>$this->params->get('username'),
+            'pass'=>$this->params->get('password'),
+            'Content-Type'=>'multipart/form-data');
 
-    public function onJHarvestIngest($harvest)
-    {
-        $params = new JRegistry($harvest->params);
+        $post = array(
+            'upload'=>
+                curl_file_create($path, 'application/zip', JFile::getName($path)));
 
-        $this->harvestId = $harvest->id;
+        $url = new JUri($this->params->get('rest_url').'/items.stream');
+        $response = $http->post($url, $post, $headers);
 
-        $items = $this->getCache(0);
-
-        $i = count($items);
-
-        $temp = 0;
-
-        while (count($items) > 0) {
-            foreach ($items as $item) {
-                $data = json_decode($item->data);
-
-                $metadata = $data->metadata;
-                $assets = $data->assets;
-
-                if (!isset($metadata->{"dc.type"})) {
-                    $metadata->{"dc.type"} = array("-");
-                }
-
-                if (!isset($metadata->{"dc.description"})) {
-                    $metadata->{"dc.description"} = array("-");
-                }
-
-                $collection = $params->get('ingest.dspace.collection');
-
-                $path = $this->buildPackage($item->id, $collection, $metadata, $assets);
-
-                $http = JHttpFactory::getHttp(null, 'curl');
-
-                $headers = array(
-                    'user'=>$this->params->get('username'),
-                    'pass'=>$this->params->get('password'),
-                    'Content-Type'=>'multipart/form-data');
-
-                $post = array(
-                    'upload'=>
-                        curl_file_create($path, 'application/zip', JFile::getName($path)));
-
-                $url = new JUri($this->params->get('rest_url').'/items.stream');
-                $response = $http->post($url, $post, $headers);
-
-                if ($response->code == '201') {
-                    fwrite(STDOUT, "item created: ".(string)$response->body."\n");
-                } else {
-                    fwrite(STDOUT, print_r($response, true)."\n");
-                }
-
-                JFile::delete($path);
-            }
-
-            $items = $this->getCache($i);
-            $i+=count($items);
+        if ($response->code == '201') {        
+            fwrite(STDOUT, "item created: ".(string)$response->body."\n");
+        } else {
+            fwrite(STDOUT, print_r($response, true)."\n");
         }
+
+        //JFile::delete($path);
     }
     
     /**
@@ -166,11 +113,15 @@ class PlgIngestDSpace extends JPlugin
         $request->collectionId = new SimpleXMLElement("<collectionId>".(int)$collection."</collectionId>");
         $request->metadata = new SimpleXMLElement("<metadata/>");
 
+        $registry = new \Joomla\Registry\Registry;
+        $registry->loadFile(__DIR__."/crosswalk.json", "JSON");
+        
         $i = 0;
         foreach ($metadata as $key=>$field) {
             foreach ($field as $value) {
                 $element = $request->metadata->addChild("field");
-                $element->name = $key;
+                
+                $element->name = $registry->get($key);
                 $element->value = $value;
                 $i++;
             }
@@ -209,7 +160,7 @@ class PlgIngestDSpace extends JPlugin
         $package = JArchive::getAdapter('zip');
         $package->create($zip, $files);
 
-        JFolder::delete($path);
+        //JFolder::delete($path);
 
         return $zip;
     }
@@ -222,16 +173,25 @@ class PlgIngestDSpace extends JPlugin
      */
     private function download($src, $dest)
     {
-        if ($shandle = @fopen($src, 'r')) {
-            $dhandle = fopen($dest, 'w');
+        $fp = fopen($dest, 'w+');
+    
+        //@TODO preferred method when joomla curl transport can handle bool(true).
+        //$http = JHttpFactory::getHttp(null, 'curl');
+        //$http->setOption("transport.curl", [CURLOPT_FILE=>$fp]);
+        //$http->get($src, null, 50);
 
-            while (!feof($shandle)) {
-                $chunk = fread($shandle, 1024);
-                fwrite($dhandle, $chunk);
-            }
+        set_time_limit(0);
+        $fp = fopen($dest, 'w+');
 
-            fclose($dhandle);
-            fclose($shandle);
-        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $src);
+        curl_setopt($ch, CURLOPT_FILE, $fp);  
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTPGET, true);
+
+        curl_exec($ch);
+
+        curl_close($ch);
+        fclose($fp);
     }
 }
---
+++ plugins/ingest/dspace/fields/collections.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package     Ingest.DSpace.Plugin
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2015-2017 KnowledgeArc Ltd. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+JLoader::import('joomla.form.helper');
+JFormHelper::loadFieldClass('list');
+
+/**
+ * @package     Ingest.DSpace.Plugin
+ * @subpackage  Form
+ *
+ * A class for generating a DSpace collections drop down.
+ */
+class IngestDSpaceFormFieldCollections extends \JFormFieldList
+{
+    protected $type = 'IngestDSpace.Collections';
+
+    /**
+     * Uses the configured DSpace Collections API endpoint to retrieve a list 
+     * of DSpace collections.
+     */
+    protected function getOptions()
+    {
+        // Initialize variables.
+        $options = parent::getOptions();
+        
+        $http = JHttpFactory::getHttp();
+
+        $plugin = JPluginHelper::getPlugin("ingest", "dspace");
+        $params = new JRegistry($plugin->params);
+
+        $headers = array(
+            'user'=>$params->get('username'),
+            'pass'=>$params->get('password'));
+        
+        $url = new JUri($params->get('rest_url').'/collections.json');
+        
+        $response = $http->get((string)$url, $headers);
+
+        if ($response->code === 200) {
+            $data = json_decode($response->body);
+
+            foreach ($data->collections as $collection) {
+                $options[$collection->id] = $collection->name;
+            }
+        }
+        
+        return $options;
+    }
+}
